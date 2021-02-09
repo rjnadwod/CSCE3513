b33883799a7747ec8f532da5a0ce05520b13e13b
Riley Nadwodny <72773901+rjnadwod@users.noreply.github.com>
2021-02-09 16:11:47 -0600

    Delete RegisterApp-NodeJS directory
    Remove folder from repo

--- RegisterApp-NodeJS/src/routes/productListingRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { RouteLookup } from "../controllers/lookups/routingLookup";
-import * as ProductListingRouteController from "../controllers/productListingRouteController";
-
-function productListingRoutes(server: express.Express) {
-	server.get(RouteLookup.ProductListing, ProductListingRouteController.start);
-}
-
-module.exports.routes = productListingRoutes;
--- RegisterApp-NodeJS/src/server.ts
+++
@@ -1,5 +0,0 @@
-import app from "./app";
-
-app.listen(app.get("port"), () => {
-	console.log("Listening on port " + app.get("port"));
-});
