21763aed444ac374fd31c042ae0c8b2f5a9e21cf
danielmiao00 <61894311+danielmiao00@users.noreply.github.com>
2021-04-09 17:57:01 -0500

    Update activeEmployeeExistsQuery.ts

--- src/controllers/commands/employees/activeEmployeeExistsQuery.ts
+++ src/controllers/commands/employees/activeEmployeeExistsQuery.ts
@@ -4,10 +4,12 @@ import * as EmployeeRepository from "../models/employeeModel";
 import { Resources, ResourceKey } from "../../../resourceLookup";
 
 export const query = async (): Promise<CommandResponse<boolean>> => {
+	//Query if employee is in db by ID
 	return EmployeeRepository.queryActiveExists()
 		.then((queriedEmployee: (EmployeeModel | null)): CommandResponse<boolean> => {
 			if (!queriedEmployee) {
 				return <CommandResponse<boolean>>{
+					//Return 404 if employee is not found in database					
 					status: 404,
 					message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND)
 				};
