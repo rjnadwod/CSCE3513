b7d1990e91a2f35e52ac2e9294e7b10c58d66f86
paytsmith <77457277+paytsmith@users.noreply.github.com>
2021-04-09 17:33:18 -0500

    Update signInRoutes.ts

--- src/routes/signInRoutes.ts
+++ src/routes/signInRoutes.ts
@@ -3,13 +3,15 @@ import { RouteLookup } from "../controllers/lookups/routingLookup";
 import * as SignInRouteController from "../controllers/signInRouteController";
 
 function signInRoutes(server: express.Express) {
+	// open sign in route controller (PS)
 	server.get(RouteLookup.SignIn, SignInRouteController.start);
-
+	
+	// Maggie, look at this post request. Does it look correct?
 	server.post(RouteLookup.SignIn, SignInRouteController.signIn);
 
 	server.delete(
 		(RouteLookup.API + RouteLookup.SignOut),
-		SignInRouteController.clearActiveUser);
+		SignInRouteController.clearActiveUser);		// clearing user when sign out (PS)
 }
 
 module.exports.routes = signInRoutes;
