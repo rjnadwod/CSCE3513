8cb5ca9dc694bd8cd2bd00d977c28efaf27d6299
paytsmith <77457277+paytsmith@users.noreply.github.com>
2021-04-09 17:17:03 -0500

    Update employeeDetailRoutes.ts

--- src/routes/employeeDetailRoutes.ts
+++ src/routes/employeeDetailRoutes.ts
@@ -3,21 +3,22 @@ import { RouteLookup } from "../controllers/lookups/routingLookup";
 import * as EmployeeDetailRouteController from "../controllers/employeeDetailRouteController";
 
 function employeeDetailRoutes(server: express.Express) {
+	// getting employee details (PS)
 	server.get(
 		RouteLookup.EmployeeDetail,
 		EmployeeDetailRouteController.start);
 	server.get(
 		(RouteLookup.EmployeeDetail + RouteLookup.EmployeeIdParameter),
 		EmployeeDetailRouteController.startWithEmployee);
-
+	// performing a patch request if employee exists in database (PS)
 	server.patch(
 		(RouteLookup.API + RouteLookup.EmployeeDetail
 			+ RouteLookup.EmployeeIdParameter),
-		EmployeeDetailRouteController.updateEmployee);
-
+		EmployeeDetailRouteController.updateEmployee);	// updating the employee if already exists (PS)
+	// performing a post request if the employee is new
 	server.post(
 		(RouteLookup.API + RouteLookup.EmployeeDetail),
-		EmployeeDetailRouteController.createEmployee);
+		EmployeeDetailRouteController.createEmployee);	// creating employee if new (PS)
 }
 
 module.exports.routes = employeeDetailRoutes;
