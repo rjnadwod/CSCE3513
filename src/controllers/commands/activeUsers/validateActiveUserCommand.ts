0ff1fcbdb890509092d07d0ff33b65151afbaae1
Zachary-Chapman <55763800+Zachary-Chapman@users.noreply.github.com>
2021-04-09 13:55:04 -0500

    Update validateActiveUserCommand.ts

--- src/controllers/commands/activeUsers/validateActiveUserCommand.ts
+++ src/controllers/commands/activeUsers/validateActiveUserCommand.ts
@@ -3,6 +3,7 @@ import { Resources, ResourceKey } from "../../../resourceLookup";
 import * as ActiveUserRepository from "../models/activeUserModel";
 import { CommandResponse, ActiveUser } from "../../typeDefinitions";
 
+//Function to determin if the active user has the proper status (Zachary C.)
 export const execute = async (sessionKey: string): Promise<CommandResponse<ActiveUser>> => {
 	return ActiveUserRepository.queryBySessionKey(sessionKey)
 		.then((queriedActiveUser: (ActiveUserModel | null)): Promise<CommandResponse<ActiveUser>> => {
